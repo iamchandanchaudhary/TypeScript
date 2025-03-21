cd3608ae5465c6e2e70f05222c49626ced7d7885
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-21 11:38:34 +0530

    Upload 01

---
+++ Practice 01.ts
@@ -0,0 +1,3 @@
+let firstName: string = "Chandan";
+
+console.log(firstName);
---
+++ Program 02.ts
@@ -0,0 +1,6 @@
+  
+  let firstName: string = "Chandan";
+  let lastName: string = " Chaudhary";
+
+  console.log(firstName + lastName);
+  
