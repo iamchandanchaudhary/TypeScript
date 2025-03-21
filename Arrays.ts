ac254b0c3e43f8a23d5328c3729c04fe703f4184
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-21 11:43:51 +0530

    Adding Array

---
+++ Arrays.ts
@@ -0,0 +1,8 @@
+    
+    const names: string[] = ["Chandan"];
+
+    names.push("Aman"); // no error
+
+    //names.push(3); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+    console.log(names);
