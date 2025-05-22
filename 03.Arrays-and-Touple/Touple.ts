48c8c6171d0d84fedae9a246f6e5164c786d892a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-05-22 11:52:59 +0530

    Improvement 05

--- 03.Arrays-and-Touple/Touple.ts
+++ 03.Arrays-and-Touple/Touple.ts
@@ -5,4 +5,4 @@ let user: [number, string] = [1, "Chandan"];
 // let user3: [number, string] = ["Chandan", 4];  // ==> Invalid because arrangement is wrong
 
 user[0].toFixed  // ==> it will suggest only all the methods of number object
-user[1].length  // ==> it will suggest only all the methods of string
+user[1].length  // ==> it will suggest only all the string methods
