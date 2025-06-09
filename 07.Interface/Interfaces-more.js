96909ff7b88113be43e4dfc1045f731b76a77f33
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-06-09 11:08:16 +0530

    Interface Improvement

---
+++ 07.Interface/Interfaces-more.js
@@ -0,0 +1,26 @@
+"use strict";
+let stu2 = {
+    dbId: 204,
+    name: "Ashok Verma",
+    age: 24,
+    startTrail: () => {
+        return "Trail Started";
+    },
+    coupne: (name) => {
+        return 10;
+    },
+    githubToken: "Chandu55"
+};
+let stu3 = {
+    dbId: 204,
+    name: "Chandan Chaudhary",
+    age: 18,
+    role: "learner",
+    startTrail: () => {
+        return "Trail Started";
+    },
+    coupne: (name) => {
+        return 10;
+    },
+    githubToken: "Chandu55"
+};
